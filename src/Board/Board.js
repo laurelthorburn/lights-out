e65b36c81f01fa0c40c97bab46089e120f86bb84
Laurel Thorburn <laurelthorburn@gmail.com>
2022-04-11 18:36:32 -0600

    Added has won logic, check that every cell is

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -80,9 +80,11 @@ class Board extends Component {
     flipCell(y, x - 1);
     flipCell(y, x + 1);
     flipCell(y - 1, x);
-    flipCell(y + 1, x); // win when every cell is turned off
+    flipCell(y + 1, x); 
+    
+    // win when every cell is turned off
     // TODO: determine is the game has been won
-    let hasWon = false;
+    let hasWon = board.every(row => row.every(cell => !cell));
 
     this.setState({ board, hasWon });
   }
@@ -90,6 +92,9 @@ class Board extends Component {
   /** Render game board or winning message. */
 
   render() {
+    if(this.state.hasWon){
+      return <h1>YOU WON!</h1>;
+    }
     // if the game is won, just show a winning msg & render nothing else
     let tblBoard = [];
     for (let y = 0; y < this.props.nrows; y++) {
