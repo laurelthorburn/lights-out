2ebb2b8b575742001e027cd2c72e90389329ab48
Laurel Thorburn <laurelthorburn@gmail.com>
2022-04-11 18:19:25 -0600

    Added initial props

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -29,8 +29,18 @@ import "./Board.css";
  **/
 
 class Board extends Component {
+  static defaultProps ={
+    nrows: 5,
+    ncols: 5, 
+    chanceLightStartsOn: 0.25
+  }
   constructor(props) {
     super(props);
+
+    this.state = {
+      hasWon: false,
+      board: this.createBoard()
+    };
 
     // TODO: set initial state
   }
@@ -40,6 +50,13 @@ class Board extends Component {
   createBoard() {
     let board = [];
     // TODO: create array-of-arrays of true/false values
+    for (let y=0; y <this.props.nrows; y++){
+      let row = [];
+      for (let x=0; x < this.props.ncols; x++){
+        row.push(Math.random() < this.props.chanceLightStartsOn)
+      }  
+    board.push.apply(row)
+  }
     return board;
   }
 
@@ -70,52 +87,13 @@ class Board extends Component {
 
   render() {
     // if the game is won, just show a winning msg & render nothing else
-
-    // TODO
-
-    // make table board
-
-    // TODO
     return (
       <table>
         <tbody>
-          <tr>
-            <td><Cell /></td>
-            <td><Cell /></td>
-            <td><Cell /></td>
-            <td><Cell /></td>
-            <td><Cell /></td>
-          </tr>
-          <tr>
-            <td><Cell /></td>
-            <td><Cell /></td>
-            <td><Cell /></td>
-            <td><Cell /></td>
-            <td><Cell /></td>
-          </tr>
-          <tr>
-            <td><Cell /></td>
-            <td><Cell /></td>
-            <td><Cell /></td>
-            <td><Cell /></td>
-            <td><Cell /></td>
-          </tr>
-          <tr>
-            <td><Cell /></td>
-            <td><Cell /></td>
-            <td><Cell /></td>
-            <td><Cell /></td>
-            <td><Cell /></td>
-          </tr>
-          <tr>
-            <td><Cell /></td>
-            <td><Cell /></td>
-            <td><Cell /></td>
-            <td><Cell /></td>
-            <td><Cell /></td>
-          </tr>
-
-        </tbody>
+      <tr>
+        
+      </tr>
+       </tbody>
       </table>
     );
   }
